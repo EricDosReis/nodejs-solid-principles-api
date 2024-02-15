37051cdd6888aa15612a8f9df9bfdb98f61283ec
EricDosReis <ericdosreis.personal@gmail.com>
2024-02-15 14:29:10 -0300

    chore: create base project

---
+++ src/env/index.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const parsedEnvSchema = envSchema.safeParse(process.env);
+
+if (parsedEnvSchema.success === false) {
+  const errorMessage = 'Invalid environment variables!';
+
+  console.error(errorMessage, parsedEnvSchema.error.format());
+
+  throw new Error(errorMessage);
+}
+
+export const env = parsedEnvSchema.data;
