277d00695414061efd41d5f56b6b373629608b5b
EricDosReis <ericdosreis.personal@gmail.com>
2024-03-04 17:25:14 -0300

    feat: implements gyms repository

--- src/repositories/gyms/gyms.ts
+++ src/repositories/gyms/gyms.ts
@@ -1,13 +1,23 @@
 import { prisma } from '@/lib/prisma';
-import type { Prisma } from '@prisma/client';
+import type { Gym, Prisma } from '@prisma/client';
 
-import type { IGymsRepository } from './types';
+import { NUMBER_OF_ITEMS_PER_PAGE } from './constants';
+import type { FindManyNearbyParams, IGymsRepository } from './types';
 
 export class GymsRepository implements IGymsRepository {
   async create(data: Prisma.GymCreateInput) {
     const gym = await prisma.gym.create({ data });
 
     return gym;
+  }
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE (6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${longitude})) + sin(radians(${latitude})) * sin(radians(latitude)))) <= 10
+    `;
+
+    return gyms;
   }
 
   async findById(id: string) {
@@ -18,5 +28,19 @@ export class GymsRepository implements IGymsRepository {
     });
 
     return gym;
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: NUMBER_OF_ITEMS_PER_PAGE,
+      skip: (page - 1) * NUMBER_OF_ITEMS_PER_PAGE,
+    });
+
+    return gyms;
   }
 }
