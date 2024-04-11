37af2e8f4810c011a8eaf885226983c535bc7b2f
EricDosReis <ericdosreis.personal@gmail.com>
2024-04-11 17:43:05 -0300

    feat: create gyms controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,26 @@
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+import { makeGetNearbyGymsUseCase } from '@/use-cases/factories/make-get-nearby-gyms-use-case';
+
+export async function nearby(request: FastifyRequest, response: FastifyReply) {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine(value => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine(value => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body);
+
+  const getNearbyGymsUseCase = makeGetNearbyGymsUseCase();
+
+  const { gyms } = await getNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return response.status(200).send({ gyms });
+}
